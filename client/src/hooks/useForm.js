c347df80c934c3ca0b399caa7804c04d7acaca07
Lynda Santiago <you@example.com>
2020-08-07 12:20:14 -0400

    plants are being rendered successfully

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,13 @@
 // write your custom hook here to control your checkout form
+
+import { useState } from 'react'
+import CheckoutForm from '../components/CheckoutForm'
+
+export const useForm = ()=>{
+    const[values, setValues]=useState()
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+      return[handleChanges]
+}
